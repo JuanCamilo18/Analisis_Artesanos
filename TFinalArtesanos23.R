02b4aeb26c9ee0d7624e00498353b3aff053e383
Juan18 <juan_camilo1018@hotmail.com>
2023-03-27 04:21:43 -0500

    add graficos v2

--- TFinalArtesanos23.R
+++ TFinalArtesanos23.R
@@ -35,6 +35,13 @@ summary(df$UBIGEO)
 table(df$UBIGEO)
 
 
+
+#### Analisis descriptivo ####
+
+## Estructura del dataframe 
+str(df)
+
+
 ## Analicemos el numero de artesanos por provincia 
 Num_Artesanos <- df %>% 
   group_by(PROVINCIA) %>% 
@@ -42,20 +49,65 @@ Num_Artesanos <- df %>%
   # ordenar el dataframe por la columna PRomedioMOnto
   arrange(desc(N_Artesanos))
 
+
 Num_Artesanos %>% 
-  ggplot(mapping = aes(x = PROVINCIA , y = N_Artesanos))+
-  geom_col() +
-  # geom_bar(stat = "identity") +
-  theme(axis.text.x = element_text(angle = 90))
+  ggplot(mapping = aes(x = reorder(PROVINCIA, desc(N_Artesanos)), y = N_Artesanos)) +
+  #geom_col() +
+  geom_bar(stat = "identity", fill = "red", alpha = 0.6, width = 0.8)+
+  theme(axis.text.x = element_text(angle = 45, face = "italic", colour = "blue"))+
+  #coord_flip()+
+  geom_text(aes(label = N_Artesanos), vjust = -0.3) +
+  labs(x="Provincias",y= "Numero de artesanos")  +              
+  #ylim(c(0,300)) +
+  ggtitle("Diagrama de barras: Numero de artesanos por provincia") 
+
 
 
 ## Analicemos el numero de artesanos por distrito
+Num_Art2 <- df %>% 
+  group_by(DISTRITO) %>% 
+  summarize(N_Artesanos = n()) %>% 
+  # ordenar el dataframe por la columna PRomedioMOnto
+  arrange(desc(N_Artesanos))
+
+
+Num_Art2 %>% 
+  ggplot(mapping = aes(x = reorder(DISTRITO, desc(N_Artesanos)), y = N_Artesanos)) +
+  #geom_col() +
+  geom_bar(stat = "identity", fill = "red", alpha = 0.6, width = 0.8)+
+  theme(axis.text.x = element_text(angle = 90, face = "italic", colour = "blue"))+
+  #coord_flip()+
+  geom_text(aes(label = N_Artesanos), vjust = -0.3) +
+  labs(x="Distrito",y= "Numero de artesanos")  +              
+  #ylim(c(0,300)) +
+  ggtitle("Diagrama de barras: Numero de artesanos por distrito") 
+
 
 
 ## Analicemos la inscripcion de artesanos por clase solicitante
 summary(df$SOLICITANTE)
 table(df$SOLICITANTE)
 unique(df$SOLICITANTE)
+
+
+Art_sol <- df %>% 
+  group_by(SOLICITANTE) %>% 
+  summarize(N_Artesanos = n()) %>% 
+  # ordenar el dataframe por la columna PRomedioMOnto
+  arrange(desc(N_Artesanos))
+
+
+Art_sol %>% 
+  ggplot(mapping = aes(x = reorder(SOLICITANTE, desc(N_Artesanos)), y = N_Artesanos)) +
+  #geom_col() +
+  geom_bar(stat = "identity", fill = "red", alpha = 0.6, width = 0.8)+
+  theme(axis.text.x = element_text(angle = 15, face = "italic", colour = "blue"))+
+  #coord_flip()+
+  geom_text(aes(label = N_Artesanos), vjust = -0.3) +
+  labs(x="Numero de artesanos",y= "Solicitante")  +              
+  #ylim(c(0,300)) +
+  ggtitle("Numero de artesanos segun tipo solicitante")
+
 
 ## Analicemos el numero de artesanos segun su estado 
 unique(df$ESTADO)
